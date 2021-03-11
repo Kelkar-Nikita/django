434853100d1179cca255f78cca18b39e6d419cb4
Kelkar-Nikita <nkkelkar100@gmail.com>
2021-03-11 22:58:39 +0530

    add html file

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -116,10 +116,10 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.1/howto/static-files/
+# https:/Sdocs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIR =[
-    os.path.join(BASE_DIR,"django\static")
+STATICFILES_DIRS =[
+    os.path.join(BASE_DIR,"static")
 ]
-STATIC_ROOT =os.path.join(BASE_DIR,"mysite/assets")
+STATIC_ROOT =os.path.join(BASE_DIR,"assets")
